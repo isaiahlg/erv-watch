d357dad555dde208060f34be22cb147fb98b16d1
Isaiah Lyons-Galante <ilyonsg@nrel.gov>
2023-10-04 17:40:57 -0600

    Add pure bus layer, fix Voronoi layer.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import DeckGL from '@deck.gl/react';
 import {GeoJsonLayer} from '@deck.gl/layers';
 import {H3HexagonLayer, S2Layer} from '@deck.gl/geo-layers';
 import {interpolateRdBu} from 'd3-scale-chromatic';
-import LINES_URL from '/data/json/lines.geojson';
-import BUSES_URL from '/data/json/buses.geojson';
+import LINES_URL from '/data/json/lines.geo.json';
+import BUSES_URL from '/data/json/buses.geo.json';
 import H3_URL from '/data/json/h3r10.json';
 import S2_URL from '/data/json/s2r16.json';
-import VORONOI_URL from '/data/json/voronoi.geojson';
+import VORONOI_URL from '/data/json/voronoi.geo.json';
 
 // style map
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
@@ -35,13 +35,38 @@ export default function App({
   mapStyle = MAP_STYLE
 }) {
   const [viewLines, toggleLines] = useState(true);
-  const [viewBuses, toggleBuses] = useState(true);
+  const [viewBuses, toggleBuses] = useState(false);
+  const [viewGlyphs, toggleGlyphs] = useState(true);
   const [viewH3, toggleH3] = useState(false);
   const [viewS2, toggleS2] = useState(false);
   const [viewVoronoi, toggleVoronoi] = useState(false);
 
-  const busesLayer = new GeoJsonLayer({
-      id: 'buses',
+  const lineLayer = new GeoJsonLayer({
+    id: 'lines',
+    data: lines,
+    opacity: 0.1,
+    getLineColor: [255, 255, 255],
+    getLineWidth: 2,
+    pickable: true,
+    visible: viewLines
+  })
+
+  const busLayer = new GeoJsonLayer({
+    id: 'buses',
+    data: buses,
+    opacity: 0.2,
+    filled: true,
+    pointType: 'circle',
+    radiusUnits: 'meters',
+    getPointRadius: 3,
+    getFillColor: [255, 255, 255],
+    getLineColor: [255, 255, 255],
+    pickable: true,
+    visible: viewBuses
+  })
+
+  const glyphLayer = new GeoJsonLayer({
+      id: 'glyphs',
       data: buses,
       opacity: 0.8,
       filled: true,
@@ -52,23 +77,13 @@ export default function App({
       getFillColor: f => COLOR_SCALE(-20*(f.properties.voltage-1) + 0.5),
       getLineColor: f => COLOR_SCALE(-20*(f.properties.voltage-1) + 0.5),
       pickable: true,
-      visible: viewBuses
+      visible: viewGlyphs
     })
-  
-  const linesLayer = new GeoJsonLayer({
-    id: 'lines',
-    data: lines,
-    opacity: 0.1,
-    getLineColor: [255, 255, 255],
-    getLineWidth: 2,
-    pickable: true,
-    visible: viewLines
-  })
 
   const h3Layer = new H3HexagonLayer({
     id: 'h3-1',
     data: hexes,
-    opacity: 0.5,
+    opacity: 0.6,
     filled: true,
     extruded: false,
     getHexagon: d => d.h3,
@@ -80,7 +95,7 @@ export default function App({
   const s2Layer = new S2Layer({
     id: 's2-1',
     data: s2tiles,
-    opacity: 0.5,
+    opacity: 0.6,
     filled: true,
     extruded: false,
     getS2Token: d => d.s2,
@@ -92,7 +107,7 @@ export default function App({
   const voronoiLayer = new GeoJsonLayer({
     id: 'voronoi',
     data: voronoi,
-    opacity: 0.8,
+    opacity: 0.6,
     filled: true,
     pointType: 'circle',
     pointRadiusMaxPixels: 20,
@@ -123,36 +138,42 @@ export default function App({
           voronoiLayer,
           s2Layer,
           h3Layer,
-          busesLayer,
-          linesLayer
+          glyphLayer,
+          busLayer,
+          lineLayer
         ]}
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
         getTooltip={getTooltip}
       >
         <button 
-          onClick = {() => toggleBuses(!viewBuses)}
-          style = {buttonStyle(viewBuses, 0)}> 
-          Buses
-        </button>
-        <button 
           onClick = {() => toggleLines(!viewLines)}
-          style = {buttonStyle(viewLines, 1)}> 
+          style = {buttonStyle(viewLines, 0)}> 
           Lines
         </button>
         <button 
+          onClick = {() => toggleBuses(!viewBuses)}
+          style = {buttonStyle(viewBuses, 0.9)}> 
+          Buses
+        </button>
+        <button 
+          onClick = {() => toggleGlyphs(!viewGlyphs)}
+          style = {buttonStyle(viewGlyphs, 1.9)}> 
+          Glyphs
+        </button>
+        <button 
           onClick = {() => toggleH3(!viewH3)}
-          style = {buttonStyle(viewH3, 2)}> 
+          style = {buttonStyle(viewH3, 2.9)}> 
           H3 Hexes
         </button>
         <button 
           onClick = {() => toggleS2(!viewS2)}
-          style = {buttonStyle(viewS2, 4)}> 
+          style = {buttonStyle(viewS2, 4.2)}> 
           S2 Tiles
         </button>
         <button 
           onClick = {() => toggleVoronoi(!viewVoronoi)}
-          style = {buttonStyle(viewVoronoi, 4)}> 
+          style = {buttonStyle(viewVoronoi, 5.3)}> 
           Voronoi
         </button>
         <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
