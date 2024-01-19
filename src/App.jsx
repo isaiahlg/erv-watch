452da8ec3789aa2c96b3ed62d950ae52dfee9456
Isaiah Lyons-Galante <ilyonsg@nrel.gov>
2024-01-18 18:01:28 -0700

    Add in animation to main branch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Map from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 import DeckGL from '@deck.gl/react';
@@ -6,11 +6,12 @@ import {GeoJsonLayer} from '@deck.gl/layers';
 import {H3HexagonLayer, S2Layer} from '@deck.gl/geo-layers';
 import {GridLayer} from '@deck.gl/aggregation-layers';
 import {interpolateRdBu, interpolateRdPu} from 'd3-scale-chromatic';
+import {csv} from 'd3-fetch';
 
 const LINES_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/lines.geo.json';
 const BUSES_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/buses.geo.json';
 const H3_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/h3r10.json';
-const S2_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/s2r16.json';
+const S2_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/s2r17.json';
 const VORONOI_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/voronoi.geo.json';
 const CONTOUR_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/contours.json'
 const EV_STATIONS_URL = 'https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/json/evstations.geo.json'
@@ -45,11 +46,62 @@ export default function App() {
   const [viewVoronoi, toggleVoronoi] = useState(true);
   const [viewContours, toggleContours] = useState(false);
   const [viewCurrents, toggleCurrents] = useState(false);
-  const [viewEVStations, toggleEVStations] = useState(true);
+  const [viewEVStations, toggleEVStations] = useState(false);
   const [viewPV, togglePV] = useState(false);
-  const [viewStorage, toggleStorage] = useState(true);
-  const [viewTX, toggleTX] = useState(true);
+  const [viewStorage, toggleStorage] = useState(false);
+  const [viewTX, toggleTX] = useState(false);
+  
+  const [timestep, setTimestep] = useState(0);
+  const [allVoltages, setAllVoltages] = useState([]);
+  const [times, setTimes] = useState([]);
+  const [currentVoltages, setCurrentVoltages] = useState([]);
+  
+  const [animate, setAnimate] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [speed, setSpeed] = useState(1);
 
+  // effect to fetch all data at the start of the app
+  useEffect(() => { 
+    const fetchData = async () => {
+      setLoading(true);
+      // const data = await csv('/data/csv/bus_voltages_all.csv');
+      const data = await csv('https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/csv/bus_voltages_all.csv');
+      console.log("Fetched the full voltage data: ", data)
+      setAllVoltages(data);
+
+      // const t = await csv('/data/csv/time_steps.csv');
+      const t = await csv('https://raw.githubusercontent.com/geohai/vite-vis-dss/main/data/csv/time_steps.csv');
+      console.log("Fetched timesteps", t)
+      setTimes(t);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // effect to fetch Voltages for current timestep
+  useEffect(() => {
+    const getVoltagesByTimestep = (timestep) => {
+      return allVoltages[timestep];
+    };
+    setCurrentVoltages(getVoltagesByTimestep(timestep));
+  }, [allVoltages, timestep]);
+  
+  // function to update the timestep when buttons are clicked
+  const stepTime = (steps) => {
+    setTimestep(timestep + steps % 288);
+  };
+
+  // effect to animate the timestep
+  useEffect(() => {
+    if (animate) {
+      const interval = setInterval(() => {
+        setTimestep((timestep + 1) % 288)
+      }, 1000/speed)
+      return () => clearInterval(interval)
+    }
+  }, [animate, timestep, speed])
+
+  // layers
   const lineLayer = new GeoJsonLayer({
     id: 'lines',
     data: LINES_URL,
@@ -82,8 +134,18 @@ export default function App() {
       pointType: 'circle',
       pointRadiusMaxPixels: 50,
       radiusUnits: 'meters',
-      getPointRadius: f => 400 * Math.abs(f.properties.voltage-1),
-      getFillColor: f => RDBU_COLOR_SCALE(-20*(f.properties.voltage-1) + 0.5),
+      getPointRadius: f => {
+        var voltage = +currentVoltages[f.properties.bus]
+        return 400 * Math.abs(voltage-1)
+      },
+      getFillColor: f => {
+        var voltage = +currentVoltages[f.properties.bus]
+        return RDBU_COLOR_SCALE(-20*(voltage-1) + 0.5)
+      },
+      updateTriggers: {
+        getFillColor: [currentVoltages],
+        getPointRadius: [currentVoltages]
+      },
       getLineWidth: 0,
       pickable: true,
       visible: viewGlyphs
@@ -118,7 +180,13 @@ export default function App() {
     data: VORONOI_URL,
     opacity: 0.6,
     filled: true,
-    getFillColor: f => RDBU_COLOR_SCALE(-20*(f.properties.voltage-1) + 0.5),
+    getFillColor: f => {
+      var voltage = +currentVoltages[f.properties.bus]
+      return RDBU_COLOR_SCALE(-20*(voltage-1) + 0.5)
+    },
+    updateTriggers: {
+      getFillColor: [currentVoltages],
+    },
     getLineWidth: 0,
     pickable: true,
     visible: viewVoronoi
@@ -240,13 +308,52 @@ export default function App() {
     visible: viewTX
   })
 
-  const buttonStyle = (view, n) => {
+  const layerButtonStyle = (view, n) => {
     return ({
     position: 'absolute', 
     left: 5 + 75*n, 
     top: 5,
     color: 'white',
     backgroundColor: view ? 'blue' : 'gray',
+    border: 'none',
+    padding: '10px',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px 0 rgba(255, 255, 255, 0.2)'
+  })};
+
+  const infoButtonStyle = (n) => {
+    return ({
+    position: 'absolute', 
+    right: 5 + 75*n, 
+    top: 5,
+    color: 'white',
+    backgroundColor: 'black',
+    border: 'none',
+    padding: '10px',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px 0 rgba(255, 255, 255, 0.2)'
+  })};
+
+  const skipButtonStyle = (n) => {
+    return ({
+    position: 'absolute', 
+    right: 5 + 75*n, 
+    top: 5,
+    color: 'white',
+    backgroundColor: 'green',
+    border: 'none',
+    padding: '10px',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px 0 rgba(255, 255, 255, 0.2)'
+  })};
+
+  const playButtonStyle = (n) => {
+    return ({
+    position: 'absolute', 
+    right: 5 + 75*n, 
+    top: 5,
+    color: 'white',
+    backgroundColor: animate ? 'red' : 'green',
     border: 'none',
     padding: '10px',
     borderRadius: '10px',
@@ -275,64 +382,102 @@ export default function App() {
       >
         <button 
           onClick = {() => toggleLines(!viewLines)}
-          style = {buttonStyle(viewLines, 0)}> 
+          style = {layerButtonStyle(viewLines, 0)}> 
           Lines
         </button>
         <button 
           onClick = {() => toggleBuses(!viewBuses)}
-          style = {buttonStyle(viewBuses, 0.9)}> 
+          style = {layerButtonStyle(viewBuses, 0.9)}> 
           Buses
         </button>
         <button 
           onClick = {() => toggleGlyphs(!viewGlyphs)}
-          style = {buttonStyle(viewGlyphs, 1.9)}> 
-          Glyphs
+          style = {layerButtonStyle(viewGlyphs, 1.9)}> 
+          Glyphs*
         </button>
         <button 
           onClick = {() => toggleH3(!viewH3)}
-          style = {buttonStyle(viewH3, 2.9)}> 
+          style = {layerButtonStyle(viewH3, 2.9)}> 
           H3 Hexes
         </button>
         <button 
           onClick = {() => toggleS2(!viewS2)}
-          style = {buttonStyle(viewS2, 4.2)}> 
+          style = {layerButtonStyle(viewS2, 4.2)}> 
           S2 Tiles
         </button>
         <button 
           onClick = {() => toggleVoronoi(!viewVoronoi)}
-          style = {buttonStyle(viewVoronoi, 5.3)}> 
-          Voronoi
+          style = {layerButtonStyle(viewVoronoi, 5.3)}> 
+          Voronoi*
         </button>
         <button 
           onClick = {() => toggleContours(!viewContours)}
-          style = {buttonStyle(viewContours, 6.4)}> 
+          style = {layerButtonStyle(viewContours, 6.4)}> 
           Contours
         </button>
         <button 
           onClick = {() => toggleCurrents(!viewCurrents)}
-          style = {buttonStyle(viewCurrents, 7.6)}> 
+          style = {layerButtonStyle(viewCurrents, 7.6)}> 
           Currents
         </button>
         <button 
           onClick = {() => toggleEVStations(!viewEVStations)}
-          style = {buttonStyle(viewEVStations, 8.8)}> 
+          style = {layerButtonStyle(viewEVStations, 8.8)}> 
           EV Stations
         </button>
         <button 
           onClick = {() => togglePV(!viewPV)}
-          style = {buttonStyle(viewPV, 10.2)}> 
+          style = {layerButtonStyle(viewPV, 10.2)}> 
           PV
         </button>
         <button 
           onClick = {() => toggleStorage(!viewStorage)}
-          style = {buttonStyle(viewStorage, 10.9)}> 
+          style = {layerButtonStyle(viewStorage, 10.9)}> 
           Storage
         </button>
         <button 
           onClick = {() => toggleTX(!viewTX)}
-          style = {buttonStyle(viewTX, 12)}> 
+          style = {layerButtonStyle(viewTX, 12)}> 
           Transformers
         </button>
+        <button 
+          onClick = {() => stepTime(1)}
+          style = {skipButtonStyle(0)}> 
+          ►►
+        </button>
+        <button 
+          onClick = {() => setAnimate(!animate)}
+          style = {playButtonStyle(0.7)}> 
+          {animate ? "■" : "►"}
+        </button>
+        <button 
+         onClick = {() => stepTime(-1)}
+          style = {skipButtonStyle(1.25)}> 
+          ◄◄
+        </button>
+        <button 
+         onClick = {() => setSpeed(speed === 1 ? speed : speed / 2)}
+          style = {skipButtonStyle(1.97)}> 
+          ▼
+        </button>
+        <button 
+         onClick = {() => setSpeed(speed * 2)}
+          style = {skipButtonStyle(2.5)}> 
+          ▲
+        </button>
+        <button 
+          style = {infoButtonStyle(3.1)}> 
+          Speed: {speed}x
+        </button>
+        <button 
+          style = {infoButtonStyle(4.4)}> 
+          Timestep: {timestep}
+        </button>
+        <button 
+          style = {infoButtonStyle(5.8)}> 
+          {times[timestep].time}
+        </button>
+        <img src="public/loading.gif" style={{position: 'absolute', left: '45%', top: '30%', width: 200, height: 200, opacity: 0.5, display: loading ? 'block' : 'none'}} />
         <Map reuseMaps mapLib={maplibregl} mapStyle={MAP_STYLE} preventStyleDiffing={true} />
       </DeckGL>
   );
